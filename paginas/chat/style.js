b531942386b57ffb5385f5c3d1a89178a2913d7e
Marina-ADS <72274697+Marina-ADS@users.noreply.github.com>
2020-11-22 20:14:44 -0300

    Update style.js

--- paginas/chat/style.js
+++ paginas/chat/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.View
   background-color:#AB1B71;
   flex:1;
   padding:10px 10px 0px 10px;
@@ -8,33 +8,30 @@ export const Container = styled.View`
   justify-content: flex-end;
   align-items:center;
   margin-top:25px;
-`;
 
 
-export const Texto = styled.Text`
+export const Texto = styled.Text
  color:#fff;
  font-size:20px;
  text-align:center;
-`
 
-export const ContainerMessages = styled.View`
+export const ContainerMessages = styled.View
   width:100%;
   flex:1;
-`;
 
-export const Message = styled.Text`
+export const Message = styled.Text
  color:#fff;
  font-size:14px;
  text-align:justify;
  padding-top:5px;
-`
 
-export const ContainerButtons = styled.View`
+
+export const ContainerButtons = styled.View
   flex-direction:row;
   margin-bottom:10px;
-`
 
-export const Button = styled.TouchableOpacity`
+
+export const Button = styled.TouchableOpacity
   flex:1;
   width:100%;
   height: 60px;
@@ -44,15 +41,15 @@ export const Button = styled.TouchableOpacity`
   justify-content:center;
   align-items:center;
   margin-right:${props => props.invert ? "10px" : "0px"};
-`
 
-export const ButtonText = styled.Text`
+
+export const ButtonText = styled.Text
   color:${props => props.invert ? "#ae1b73" : "#fff"};
   font-size:16px;
   font-weight:bold;
-  `
 
-export const Input = styled.TextInput`
+
+export const Input = styled.TextInput
   border:1px solid #ccc;
   height:60px;
   flex:3;
